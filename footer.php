a5b0154d409e8f44d00bf3d74454f4065d67a0c4
cseyz <49180474+cseyz@users.noreply.github.com>
2020-03-17 17:10:52 +0800

    Section 3 PHP

---
+++ footer.php
@@ -0,0 +1,21 @@
+    <footer class="container-fluid footerbg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center footer-content">
+                    <p><?php bloginfo('name'); ?></p>
+                    <img src="http://206.189.45.97/~mesh15/wp-content/uploads/2020/03/tumblr.png" alt=“tumblr”>
+                    <img src="http://206.189.45.97/~mesh15/wp-content/uploads/2020/03/rss.png" alt=“rss”>
+                </div>
+                <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p>
+                    <img src="http://206.189.45.97/~mesh15/wp-content/uploads/2020/03/linkedin.png" alt=“linkedin”>
+                    <img src="http://206.189.45.97/~mesh15/wp-content/uploads/2020/03/fb.png" alt=“facebook”>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+<?php wp_footer(); ?>
